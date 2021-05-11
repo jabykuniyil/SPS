d9bac15a082364fb46eb584eb7b521a417281d45
jaby <mohdjabiran112@gmail.com>
2021-05-11 08:23:30 +0530

    environment

--- sps/settings.py
+++ sps/settings.py
@@ -162,5 +162,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ['mohdjabiran112@gmail.com']
-EMAIL_HOST_PASSWORD = os.environ['fuckyoubro']
+EMAIL_HOST_USER = os.environment.get('mohdjabiran112@gmail.com')
+EMAIL_HOST_PASSWORD = os.environment.get('fuckyoubro')
